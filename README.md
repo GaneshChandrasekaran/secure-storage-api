50c230ab177da8437d285c33b3f8b4f8a8ec6b8c
Ganesh Chandrasekaran <77708578+GaneshChandrasekaran@users.noreply.github.com>
2021-05-08 13:27:12 +0200

    Setup Swagger UI documentation. (#5)

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 * Run `make init` to initialize project
 
-* Open in browser: https://secure-storage.localhost/item Should get `Full authentication is required to access this resource.` error, because first you need to make `login` call (see `postman_collection.json` or `SecurityController` for more info).
+* Open in the browser: https://secure-storage.localhost/item Should get `Full authentication is required to access this resource.` error, because first you need to make `login` call (see `postman_collection.json` or `SecurityController` for more info).
 
 ### Run tests
 
@@ -28,3 +28,8 @@ make tests
 ### Postman requests collection
 
 You can import all available API calls to Postman using `postman_collection.json` file
+
+### API Documentation
+
+Open the following link in the browser to view API capabilities:
+https://secure-storage.localhost/api/doc 
--- src/Infrastructure/Api/Controller/SecurityController.php
+++ src/Infrastructure/Api/Controller/SecurityController.php
@@ -2,6 +2,12 @@
 
 namespace App\Infrastructure\Api\Controller;
 
+use Swagger\Annotations\Items;
+use Swagger\Annotations\Parameter;
+use Swagger\Annotations\Property;
+use Swagger\Annotations\Response as SwaggerResponse;
+use Swagger\Annotations\Schema;
+use Swagger\Annotations\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +16,56 @@ class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login", methods={"POST"})
+     *
+     * @Parameter(
+     *   name="payload",
+     *   in="body",
+     *   @Schema(
+     *     type="object",
+     *     @Property(property="username", type="string"),
+     *     @Property(property="password", type="string")
+     *     )
+     *   ),
+     *   required=true
+     * )
+     *
+     * @SwaggerResponse(
+     *     response=200,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="username", type="string"),
+     *       @Property(property="roles", type="array",@Items(type="string"))
+     *     )
+     * )
+     *
+     *  @SwaggerResponse(
+     *     response=400,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="error", type="string")
+     *     )
+     * )
+     *
+     *  @SwaggerResponse(
+     *     response=401,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="error", type="string")
+     *     )
+     * )
+     *
+     * @SwaggerResponse(
+     *     response=500,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="error", type="string")
+     *     )
+     * )
+     * @Tag(name="Authorization")
      */
     public function login(): JsonResponse
     {
@@ -23,6 +79,51 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/logout", name="logout", methods={"POST"})
+     *
+     * @Parameter(
+     *   name="payload",
+     *   in="body",
+     *   @Schema(
+     *     type="object",
+     *     @Property(property="username", type="string"),
+     *     @Property(property="password", type="string"),
+     *     )
+     *   ),
+     *   required=true
+     * )
+     *
+     * @SwaggerResponse(
+     *     response=200,
+     *     description=""
+     * )
+     *
+     * @SwaggerResponse(
+     *     response=400,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="error", type="string")
+     *     )
+     * )
+     *
+     *  @SwaggerResponse(
+     *     response=401,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="error", type="string")
+     *     )
+     * )
+     *
+     * @SwaggerResponse(
+     *     response=500,
+     *     description="",
+     *     @Schema(
+     *       type="object",
+     *       @Property(property="error", type="string")
+     *     )
+     * )
+     * @Tag(name="Authorization")
      */
     public function logout()
     {
